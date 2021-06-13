7f3b3c20be8f024c50a670eae3626b8147eb906f
konagaya <cresta522@gmail.com>
2021-06-13 14:56:14 +0900

    fix to refer namespace

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
         /**
          * @var User
          */
-        $user = Auth::user();
+        $user = \Auth::user();
         return view('edit', ['user' => $user]);
     }
 
