5107026b06f9dab4423fe39fb0a639520557f67c
konagaya <cresta522@gmail.com>
2021-06-16 16:24:12 +0900

    target blank

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -22,13 +22,13 @@
                                         <div class="card-header">{{ $post->user->name }}</div>
 
                                         <div class="card-body">
-                                            <a href="{{ $post->work_url }}" class="btn btn-primary">ポートフォリオ</a>
-                                            <a href="{{ $post->repo_url }}" class="btn btn-primary">リポジトリ</a>
+                                            <a href="{{ $post->work_url }}" class="btn btn-primary" target="_blank">ポートフォリオ</a>
+                                            <a href="{{ $post->repo_url }}" class="btn btn-primary" target="_blank">リポジトリ</a>
 
                                             <img src="{{ $post->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
 
                                             @if ($post->user->twitter_id )
-                                                <a href="{{ $post->user->twitter_url }}" class="btn btn-primary">Twitter</a>
+                                                <a href="{{ $post->user->twitter_url }}" class="btn btn-primary" target="_blank">Twitter</a>
                                             @else
                                                 <p>{{ $post->user->name }}</p>
                                             @endif
