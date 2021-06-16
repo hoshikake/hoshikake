245a542dadcd2ea14f44fddf74f707598da671f0
konagaya <cresta522@gmail.com>
2021-06-16 16:37:49 +0900

    プロパティ追加

--- app/User.php
+++ app/User.php
@@ -5,7 +5,10 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * @property string $name 初期値はGithubの表示名
@@ -53,5 +56,10 @@ class User extends Authenticatable
             return "https://twitter.com/" . $this->twitter_id;
         }
         return '';
+    }
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
     }
 }
