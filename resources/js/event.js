5979b5f8888221498bbe093b1a13a091ac21af42
konagaya <cresta522@gmail.com>
2021-06-18 00:54:49 +0900

    append修正

--- resources/js/event.js
+++ resources/js/event.js
@@ -4,8 +4,12 @@
 /* jshint -W117 */
 $(document).ready(() => {
     $('.btn-edit-comment').on('click', (e) => {
+        $('.edit-comment-form').remove();
         const $wrapper = $(e.target).closest('.comment-wrapper');
         const $textarea = $('<textarea></textarea>');
+        $textarea.addClass('form-control');
+        $textarea.addClass('edit-comment-form');
+
         $wrapper.append($textarea);
     });
 });
