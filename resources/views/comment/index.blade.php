e4f35e71e2ae9eeecd71d61ebab0adc07787a493
konagaya <cresta522@gmail.com>
2021-06-18 01:04:56 +0900

    html 修正

--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -58,23 +58,27 @@
 
                     @if (count($comments) > 0)
                         @foreach ($comments as $comment)
-                            <div class="border mb-1 comment-wrapper" data-id="{{ $comment->id }}">
-                                <div class="row">
-                                    <div class="col">
-                                        <p class="comment">{!! nl2br(e($comment->comment)) !!}</p>
-                                    </div>
-                                </div>
-                                <div class="row">
-                                    <div class="col mb-1">
-                                        <img src="{{ $comment->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
-                                        @if ($comment->user->twitter_id)
-                                            <a href="{{ $comment->user->twitter_url }}" class="btn btn-sm btn-primary" target="_blank">Twitter</a>
-                                        @else
-                                            <p>{{ $comment->user->name }}</p>
-                                        @endif
-                                        @if ($comment->user->id === Auth::user()->id)
-                                            <button type="submit" class="btn btn-sm btn-primary btn-edit-comment">編集</button>
-                                        @endif
+                            <div class="row">
+                                <div class="col">
+                                    <div class="border mb-1 comment-wrapper" data-id="{{ $comment->id }}">
+                                        <div class="row">
+                                            <div class="col">
+                                                <p class="comment">{!! nl2br(e($comment->comment)) !!}</p>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col mb-1">
+                                                <img src="{{ $comment->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
+                                                @if ($comment->user->twitter_id)
+                                                    <a href="{{ $comment->user->twitter_url }}" class="btn btn-sm btn-primary" target="_blank">Twitter</a>
+                                                @else
+                                                    <p>{{ $comment->user->name }}</p>
+                                                @endif
+                                                @if ($comment->user->id === Auth::user()->id)
+                                                    <button type="submit" class="btn btn-sm btn-primary btn-edit-comment">編集</button>
+                                                @endif
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
