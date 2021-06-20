73f608131186342af7b243d7b7a7f7a7fd8706b6
hosimiya7 <nerokarurin@gmail.com>
2021-06-20 12:39:54 +0900

    l

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,29 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
 
-                    <div class="card-body">
-                        <form method="POST" action="{{ route('login') }}">
-                            @csrf
+<div class="card">
+    <div class="card-header">{{ __('Login') }}</div>
 
-                            <div class="form-group row">
-                                <label for="name" class="col-sm-4 col-form-label text-md-right">SNSログイン</label>
-                                <div class="col-md-6">
-                                    <a href="{{ url('login/github') }}" class="btn btn-secondary"><i
-                                            class="fa fa-github">
-                                            GitHub</i></a>
-                                </div>
-                            </div>
-
-                        </form>
-                    </div>
+    <div class="card-body">
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="form-group row">
+                <label for="name" class="col-sm-4 col-form-label text-md-right">SNSログイン</label>
+                <div class="col-md-6">
+                    <a href="{{ url('login/github') }}" class="btn btn-secondary">
+                        <i class="fa fa-github">GitHub</i>
+                    </a>
                 </div>
             </div>
-        </div>
+
+        </form>
     </div>
+</div>
+
 @endsection
