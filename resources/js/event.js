78dce8bb46c861573636e3bdf90cf81ecba77499
hosimiya7 <nerokarurin@gmail.com>
2021-06-20 16:05:28 +0900

    l

--- resources/js/event.js
+++ resources/js/event.js
@@ -28,34 +28,34 @@ $(document).ready(() => {
 
         $wrapper.append($form);
 
-        $(function () {
-            $('span').on("click", function() {
-                if ($(this).attr('class') === 'selected') {
-                    // メニュー非表示
-                    $(this).removeClass('selected').next('ul').slideUp('fast');
-                } else {
-                    // 表示しているメニューを閉じる
-                    $('span').removeClass('selected');
-                    $('ul').hide();
-                    // メニュー表示
-                    $(this).addClass('selected').next('ul').slideDown('fast');
-              }
-            });
-
-            // マウスカーソルがメニュー上/メニュー外
-            $('span,ul').hover(function(){
-                over_flg = true;
-            }, function(){
-                over_flg = false;
-            });
-
-            // メニュー領域外をクリックしたらメニューを閉じる
-            $('body').click(function() {
-                if (over_flg == false) {
-                    $('span').removeClass('selected');
-                    $('ul').slideUp('fast');
-                }
-            });
-        });
     });
+
+    $('span').on("click", function() {
+        if ($(this).attr('class') === 'selected') {
+            // メニュー非表示
+            $(this).removeClass('selected').next('ul').slideUp('fast');
+        } else {
+            // 表示しているメニューを閉じる
+            $('span').removeClass('selected');
+            $('ul').hide();
+            // メニュー表示
+            $(this).addClass('selected').next('ul').slideDown('fast');
+        }
+    });
+
+    // マウスカーソルがメニュー上/メニュー外
+    $('span,ul').hover(function(){
+        over_flg = true;
+    }, function(){
+        over_flg = false;
+    });
+
+    // メニュー領域外をクリックしたらメニューを閉じる
+    $('body').click(function() {
+        if (over_flg == false) {
+            $('span').removeClass('selected');
+            $('ul').slideUp('fast');
+        }
+    });
+
 });
