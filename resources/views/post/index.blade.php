d7f963ed96761aad25c2e44610faa3dadb4ad49e
hosimiya7 <nerokarurin@gmail.com>
2021-06-20 22:42:52 +0900

    index layout

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -9,24 +9,24 @@
             @if (count($posts) > 0)
                 <div class="">
                     @foreach ($posts as $post)
-                         <div class="card">
-                            <div class="card-header">{{ $post->user->name }}</div>
+                         <div class="">
+                            <div class="">{{ $post->user->name }}</div>
 
-                                <div class="card-body">
+                                <div class="">
 
-                                    <a href="{{ $post->work_url }}" class="btn btn-primary" target="_blank">ポートフォリオ</a>
+                                    <a href="{{ $post->work_url }}"  target="_blank">ポートフォリオ</a>
 
-                                    <a href="{{ $post->repo_url }}" class="btn btn-primary" target="_blank">リポジトリ</a>
+                                    <a href="{{ $post->repo_url }}"  target="_blank">リポジトリ</a>
 
                                     <img src="{{ $post->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
                                     @if ($post->user->twitter_id)
-                                        <a href="{{ $post->user->twitter_url }}" class="btn btn-primary" target="_blank">Twitter</a>
+                                        <a href="{{ $post->user->twitter_url }}"  target="_blank">Twitter</a>
                                     @else
                                         <p>{{ $post->user->name }}</p>
                                     @endif
 
 
-                                    <a href="{{ route('comments.index', $post) }}" class="btn btn-primary">
+                                    <a href="{{ route('comments.index', $post) }}" >
                                         コメントをする
                                     </a>
 
