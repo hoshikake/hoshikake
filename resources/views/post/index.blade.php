db82b074b5d59744260a512072eb1047600c1e69
hosimiya7 <nerokarurin@gmail.com>
2021-06-20 23:34:00 +0900

    gallery layout

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -2,44 +2,36 @@
 
 @section('content')
 <div class="gallery-container">
-    <div class="">
-        <div class="">gallery</div>
+    <div class="gallery-title">
+        <h2>gallery</h2>
+    </div>
+    <div class="gallery-main">
+        @if (count($posts) > 0)
+            <div class="card">
+                @foreach ($posts as $post)
+                    <div class="card-header">
+                        {{ $post->user->name }}
+                    </div>
+                    <div class="gallery-link">
+                        <a href="{{ $post->work_url }}"  target="_blank">ポートフォリオ</a>
+                        <a href="{{ $post->repo_url }}"  target="_blank">リポジトリ</a>
+                        <img src="{{ $post->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
+                        @if ($post->user->twitter_id)
+                            <a href="{{ $post->user->twitter_url }}"  target="_blank">Twitter</a>
+                        @else
+                            <p>{{ $post->user->name }}</p>
+                        @endif
+                        <p >{!! nl2br(e($post->comment)) !!}</p>
 
-        <div class="">
-            @if (count($posts) > 0)
-                <div class="">
-                    @foreach ($posts as $post)
-                         <div class="">
-                            <div class="">{{ $post->user->name }}</div>
-
-                                <div class="">
-
-                                    <a href="{{ $post->work_url }}"  target="_blank">ポートフォリオ</a>
-
-                                    <a href="{{ $post->repo_url }}"  target="_blank">リポジトリ</a>
-
-                                    <img src="{{ $post->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
-                                    @if ($post->user->twitter_id)
-                                        <a href="{{ $post->user->twitter_url }}"  target="_blank">Twitter</a>
-                                    @else
-                                        <p>{{ $post->user->name }}</p>
-                                    @endif
-                                    <p >{!! nl2br(e($post->comment)) !!}</p>
-
-                                    <a href="{{ route('comments.index', $post) }}" >
-                                        コメントをする
-                                    </a>
-
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-                </div>
-            @else
-                <p>まだポートフォリオが登録されていません</p>
-            @endif
-
-        </div>
+                        <a href="{{ route('comments.index', $post) }}" >
+                            コメントをする
+                        </a>
+                    </div>
+                @endforeach
+            </div>
+        @else
+            <p>まだポートフォリオが登録されていません</p>
+        @endif
     </div>
 </div>
 @endsection
