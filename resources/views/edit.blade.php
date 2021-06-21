729f7349f2cec3c01ab31c9aceb6e60fe3aa07ec
hosimiya7 <nerokarurin@gmail.com>
2021-06-21 09:47:17 +0900

    twitter

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,25 +3,25 @@
 @section('content')
 <div class="twitter-container">
 
-    <div class="">ギャラリー</div>
-
-    <div class="">
+    <div class="card">
         @if (session('status'))
-            <div class="" role="alert">
-                {{ session('status') }}
-            </div>
+        <div class="" role="alert">
+            {{ session('status') }}
+        </div>
         @endif
 
+        <div class="card-header">
+            twitter登録
+        </div>
         <form action="{{ route('update', $user) }}" method="POST">
             @csrf
             @method('PUT')
 
             {{-- TwitterID --}}
 
-
                 <label for="twitter_id">TwitterID</label>
                 <input type="text"
-                    class="form-control {{ $errors->has('twitter_id') ? 'is-invalid' : '' }}"
+                    class="twitter-form {{ $errors->has('twitter_id') ? 'is-invalid' : '' }}"
                     id="twitter_id" name="twitter_id" value="{{ old('twitter_id', $user->twitter_id) }}">
                 @if ($errors->has('twitter_id'))
                     <div class="">
@@ -31,7 +31,7 @@
 
 
             <div class="">
-                <button type="submit" class="btn btn-primary"><i class="far fa-save"></i>
+                <button type="submit" class="twitter-btn"><i class="far fa-save"></i>
                     更新</button>
             </div>
         </form>
