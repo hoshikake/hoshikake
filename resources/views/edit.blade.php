8aae50752ff1183c8d6195b1e6fc3e7cd3868728
hosimiya7 <nerokarurin@gmail.com>
2021-06-21 09:28:58 +0900

    l

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,47 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">ギャラリー</div>
+<div class="twitter-container">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="">ギャラリー</div>
 
-                    <form action="{{ route('update', $user) }}" method="POST">
-                        @csrf
-                        @method('PUT')
-
-                        {{-- TwitterID --}}
-                        <div class="form-group row">
-                            <div class="col-4">
-                                <label for="twitter_id">TwitterID</label>
-                                <input type="text"
-                                    class="form-control {{ $errors->has('twitter_id') ? 'is-invalid' : '' }}"
-                                    id="twitter_id" name="twitter_id" value="{{ old('twitter_id', $user->twitter_id) }}">
-                                @if ($errors->has('twitter_id'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('twitter_id') }}</strong>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary"><i class="far fa-save"></i>
-                                更新</button>
-                        </div>
-                    </form>
-
-                </div>
+    <div class="">
+        @if (session('status'))
+            <div class="" role="alert">
+                {{ session('status') }}
             </div>
-        </div>
+        @endif
+
+        <form action="{{ route('update', $user) }}" method="POST">
+            @csrf
+            @method('PUT')
+
+            {{-- TwitterID --}}
+
+
+                <label for="twitter_id">TwitterID</label>
+                <input type="text"
+                    class="form-control {{ $errors->has('twitter_id') ? 'is-invalid' : '' }}"
+                    id="twitter_id" name="twitter_id" value="{{ old('twitter_id', $user->twitter_id) }}">
+                @if ($errors->has('twitter_id'))
+                    <div class="">
+                        <strong>{{ $errors->first('twitter_id') }}</strong>
+                    </div>
+                @endif
+
+
+            <div class="">
+                <button type="submit" class="btn btn-primary"><i class="far fa-save"></i>
+                    更新</button>
+            </div>
+        </form>
+
     </div>
+
 </div>
 @endsection
