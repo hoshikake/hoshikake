569d047eff5133b5a77df3349977f5eabb19d6d0
hosimiya7 <nerokarurin@gmail.com>
2021-06-21 14:19:00 +0900

    comment

--- resources/views/comment/index.blade.php
+++ resources/views/comment/index.blade.php
@@ -1,122 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">ポートフォリオ</div>
-
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-3 mb-5">
-                            <div class="card">
-                                <div class="card-header">{{ $post->user->name }}</div>
-
-                                <div class="card-body">
-                                    <div class="row">
-                                        <div class="col">
-                                            <a href="{{ $post->work_url }}" class="btn btn-primary" target="_blank">ポートフォリオ</a>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <a href="{{ $post->repo_url }}" class="btn btn-primary" target="_blank">リポジトリ</a>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col">
-                                            <img src="{{ $post->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
-                                            @if ($post->user->twitter_id )
-                                                <a href="{{ $post->user->twitter_url }}" class="btn btn-primary" target="_blank">Twitter</a>
-                                            @else
-                                                <p>{{ $post->user->name }}</p>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col">
-                            <a href="{{ route('posts.index') }}" class="btn btn-info">一覧に戻る</a>
-                        </div>
-                    </div>
-                </div>
+<div class="comment-container">
+    <div class="comment-title">ポートフォリオ</div>
+        <div class="card">
+            <div class="card-header">
+                {{ $post->user->name }}
+                <img src="{{ $post->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
             </div>
-
-            <div class="card mt-3">
-                <div class="card-header">コメント</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    @if (count($comments) > 0)
-                        @foreach ($comments as $comment)
-                            <form action="{{ route('comments.update', $comment) }}" method="post" class="form-update" data-id="{{ $comment->id }}">
-                                @csrf
-                                @method('PUT')
-                            </form>
-                            <div class="row">
-                                <div class="col border mb-1 comment-wrapper" data-id="{{ $comment->id }}">
-                                    <div class="row">
-                                        <div class="col">
-                                            <p class="comment">{!! nl2br(e($comment->comment)) !!}</p>
-                                        </div>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col mb-1">
-                                            <img src="{{ $comment->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
-                                            @if ($comment->user->twitter_id)
-                                                <a href="{{ $comment->user->twitter_url }}" class="btn btn-sm btn-primary" target="_blank">Twitter</a>
-                                            @else
-                                                <p>{{ $comment->user->name }}</p>
-                                            @endif
-                                            @if ($comment->user->id === Auth::user()->id)
-                                                <button type="submit" class="btn btn-sm btn-primary btn-edit-comment">編集</button>
-                                            @endif
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
-                    @else
-                        <p>コメントはまだありません。</p>
-                    @endif
-
-                    <p>コメントを投稿する</p>
-                    <form action="{{ route('comments.store', $post) }}" method="post">
-                        @csrf
-
-                        {{-- コメント --}}
-                        <div class="form-group row">
-                            <div class="col">
-                                <label for="comment">コメント</label>
-                                <textarea class="form-control {{ $errors->has('comment') ? 'is-invalid' : '' }}"
-                                    id="comment" name="comment" rows="3">{{ old('comment') }}</textarea>
-                                @if ($errors->has('comment'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('comment') }}</strong>
-                                    </div>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <button type="submit" class="btn btn-primary">
-                                <i class="far fa-save"></i>投稿
-                            </button>
-                        </div>
-
-                    </form>
-                </div>
+            <div class="comment-btns">
+                <a href="{{ $post->work_url }}" class="comment-btn" target="_blank">ポートフォリオ</a>
+                <a href="{{ $post->repo_url }}" class="comment-btn" target="_blank">リポジトリ</a>
+                @if ($post->user->twitter_id )
+                    <a href="{{ $post->user->twitter_url }}" class="comment-btn" target="_blank">Twitter</a>
+                @else
+                    <p>{{ $post->user->name }}</p>
+                @endif
             </div>
         </div>
+        <a href="{{ route('posts.index') }}" class="comment-btn">一覧に戻る</a>
+
+
+    <div class="">
+        <div class="">コメント</div>
+
+        <div class="card-body">
+            @if (session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+
+            @if (count($comments) > 0)
+                @foreach ($comments as $comment)
+                    <form action="{{ route('comments.update', $comment) }}" method="post" class="form-update" data-id="{{ $comment->id }}">
+                        @csrf
+                        @method('PUT')
+                    </form>
+                    <div class="row">
+                        <div class="col border mb-1 comment-wrapper" data-id="{{ $comment->id }}">
+                            <div class="row">
+                                <div class="col">
+                                    <p class="comment">{!! nl2br(e($comment->comment)) !!}</p>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col mb-1">
+                                    <img src="{{ $comment->user->avatar }}" alt="アイコン" style="height: 30px; width: 30px">
+                                    @if ($comment->user->twitter_id)
+                                        <a href="{{ $comment->user->twitter_url }}" class="btn btn-sm btn-primary" target="_blank">Twitter</a>
+                                    @else
+                                        <p>{{ $comment->user->name }}</p>
+                                    @endif
+                                    @if ($comment->user->id === Auth::user()->id)
+                                        <button type="submit" class="btn btn-sm btn-primary btn-edit-comment">編集</button>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            @else
+                <p>コメントはまだありません。</p>
+            @endif
+
+            <p>コメントを投稿する</p>
+            <form action="{{ route('comments.store', $post) }}" method="post">
+                @csrf
+
+                {{-- コメント --}}
+                <div class="form-group row">
+                    <div class="col">
+                        <label for="comment">コメント</label>
+                        <textarea class="form-control {{ $errors->has('comment') ? 'is-invalid' : '' }}"
+                            id="comment" name="comment" rows="3">{{ old('comment') }}</textarea>
+                        @if ($errors->has('comment'))
+                            <div class="invalid-feedback">
+                                <strong>{{ $errors->first('comment') }}</strong>
+                            </div>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary">
+                        <i class="far fa-save"></i>投稿
+                    </button>
+                </div>
+
+            </form>
+        </div>
     </div>
+
 </div>
 @endsection
