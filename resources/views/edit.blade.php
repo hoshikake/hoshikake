2a1875ca95fd2fb053d134f9cd42eeacbeda591c
hosimiya7 <nerokarurin@gmail.com>
2021-06-21 09:54:37 +0900

    twitter

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -13,13 +13,14 @@
         <div class="card-header">
             twitter登録
         </div>
+
         <form action="{{ route('update', $user) }}" method="POST">
             @csrf
             @method('PUT')
 
             {{-- TwitterID --}}
 
-                <label for="twitter_id">TwitterID</label>
+                <label for="twitter_id" class="twitter-text">TwitterID</label>
                 <input type="text"
                     class="twitter-form {{ $errors->has('twitter_id') ? 'is-invalid' : '' }}"
                     id="twitter_id" name="twitter_id" value="{{ old('twitter_id', $user->twitter_id) }}">
@@ -28,7 +29,6 @@
                         <strong>{{ $errors->first('twitter_id') }}</strong>
                     </div>
                 @endif
-
 
             <div class="">
                 <button type="submit" class="twitter-btn"><i class="far fa-save"></i>
